cbfed64e2adc556d8414b3c2de0296a6657cdf16
Will Vesey <willvesey2@gmail.com>
2018-02-22 01:10:37 -0500

    make run.sh work on bash

--- run.sh
+++ run.sh
@@ -14,28 +14,29 @@ function ensure_compose {
 }
 
 function ensure_basicauth {
-    sudo apt -yqq install apache2-utils 1>/dev/null 2>&1
+    sudo apt -yqq install apache2-utils
+    mkdir -p ./nginx/htpasswd/
     if [ ! -f ./nginx/htpasswd/.htpasswd_rutorrent ]; then
         pprint "Setting up rutorrent credentials"
-        print -n "New username: "
+        echo -n "New username: "
         read user
         sudo htpasswd -c ./nginx/htpasswd/.htpasswd_rutorrent $user
     fi
     if [ ! -f ./nginx/htpasswd/.htpasswd_jackett ]; then
         pprint "Setting up jackett credentials"
-        print -n "New username "
+        echo -n "New username "
         read user
         sudo htpasswd -c ./nginx/htpasswd/.htpasswd_jackett $user
     fi
     if [ ! -f ./nginx/htpasswd/.htpasswd_sonarr ]; then
         pprint "Setting up sonarr credentials"
-        print -n "New username "
+        echo -n "New username "
         read user
         sudo htpasswd -c ./nginx/htpasswd/.htpasswd_sonarr $user
     fi
     if [ ! -f ./nginx/htpasswd/.htpasswd_radarr ]; then
         pprint "Setting up radarr credentials"
-        print -n "New username "
+        echo -n "New username "
         read user
         sudo htpasswd -c ./nginx/htpasswd/.htpasswd_radarr $user
     fi
