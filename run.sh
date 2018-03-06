75dcab0a7e5b8002dfe4567fa806742bd7e0c87e
Will Vesey <willvesey2@gmail.com>
2018-03-06 14:05:50 -0500

    update run.sh, remove nginx messages

--- run.sh
+++ run.sh
@@ -15,22 +15,9 @@ function ensure_compose {
     fi
 }
 
-function ensure_basicauth {
-    sudo apt -yqq install apache2-utils
-    mkdir -p ./nginx/htpasswd/
-    if [ ! -f ./nginx/htpasswd/.htpasswd ]; then
-        pprint "Setting up credentials"
-        echo -n "New username: "
-        read user
-        sudo htpasswd -c ./nginx/htpasswd/.htpasswd $user
-    fi
-}
-
 pprint "Ensuring that docker-compose is installed"
 ensure_compose
-pprint "Ensuring nginx has basic auth enabled"
-ensure_basicauth
-pprint "Stopping existing instance, if it exists"
+pprint "Stopping current instance, if it exists"
 sudo docker-compose down -t 0 --remove-orphans
 pprint "Starting new instance"
 sudo docker-compose up -d
